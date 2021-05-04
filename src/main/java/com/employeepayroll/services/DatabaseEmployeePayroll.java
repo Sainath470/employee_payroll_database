83b169f2ca6b8a91a969639eece77c1f4d50aab7
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-05-04 22:10:54 +0530

    STARTED : welcome message to the program

---
+++ src/main/java/com/employeepayroll/services/DatabaseEmployeePayroll.java
@@ -0,0 +1,10 @@
+package com.employeepayroll.services;
+
+import java.sql.*;
+import java.util.Enumeration;
+
+public class DatabaseEmployeePayroll {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the employee payroll service jdbc program");
+    }
+}
