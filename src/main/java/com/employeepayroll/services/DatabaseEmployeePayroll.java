17b77761c05b443da2e3184b5ff1ac82bbd96104
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-05-04 22:14:10 +0530

    ADDED : created employee payroll service database

--- src/main/java/com/employeepayroll/services/DatabaseEmployeePayroll.java
+++ src/main/java/com/employeepayroll/services/DatabaseEmployeePayroll.java
@@ -5,6 +5,32 @@ import java.util.Enumeration;
 
 public class DatabaseEmployeePayroll {
     public static void main(String[] args) {
-        System.out.println("Welcome to the employee payroll service jdbc program");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "Sainath@8801";
+        Connection connection;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        }catch (ClassNotFoundException e){
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+        }
+        listDrivers();
+        try{
+            System.out.println("Connecting to database:"+jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL,userName,password);
+            System.out.println("Connection is successful!!!"+connection);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while(driverList.hasMoreElements()){
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
     }
 }
